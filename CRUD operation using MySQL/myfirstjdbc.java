ae67681b67bf13d4664b7d97c9e3f8222a3599e9
99003713 <78997716+99003713@users.noreply.github.com>
2021-03-26 14:05:57 +0530

    Update myfirstjdbc.java

--- CRUD operation using MySQL/myfirstjdbc.java
+++ CRUD operation using MySQL/myfirstjdbc.java
@@ -1,30 +1,80 @@
-//package com.ltts;
-//import java.sql.*;  
-//class myfirstjdbc{  
-//public static void main(String args[]){  
-//try{  
-//Class.forName("com.mysql.cj.jdbc.Driver");  
-//Connection con=DriverManager.getConnection(  
-//"jdbc:mysql://localhost:3306/first","root","harod@123");  
-////here first is database name, root is username and password  
-//System.out.println("Connected with DataBase");
-//System.out.println("1.Create Table\n2.Insert into table\n3.Update table\n4. Delete row  ");
-////Statement stmt=con.createStatement();  
-////stmt.execute("create table student(studentid int(3), studentname varchar(10))");
-//System.out.println("Table is created");
-//PreparedStatement stmt=con.prepareStatement("insert into student values(?,?)"); 
-//stmt.setInt(1, 2);
-//stmt.setString(2,"mithun");
-////stmt.executeUpdate();
-//
-////stmt.executeUpdate("insert into student values(1,'Om')");
-//System.out.println("Data inserted into table");
-//
-//
-//ResultSet rs=stmt.executeQuery("select * from student");  
-//while(rs.next())  
-//System.out.println(rs.getInt(1)+"  "+rs.getString(2));  
-//con.close();  
-//}catch(Exception e){ System.out.println(e);}  
-//}  
-//}
+import java.sql.*;
+import java.util.Scanner;  
+
+class App{  
+public static void main(String args[]){ 
+	Scanner sc = new Scanner(System.in);
+try{  
+Class.forName("com.mysql.cj.jdbc.Driver");
+
+System.out.println("Please Enter User ID");
+String id = sc.nextLine();
+
+System.out.println("Please Enter User PASSWORD");
+String password = sc.nextLine();
+
+Connection con=DriverManager.getConnection(  
+"jdbc:mysql://localhost:3306/first",id, password);  
+ System.out.println("Connected with DataBase");
+
+
+//stmt.execute("create table student(studentid int(3), studentname varchar(10))");  // table is already created once
+System.out.println("Table is created");
+
+ 
+ PreparedStatement stmt=con.prepareStatement("insert into student values(?,?)"); 
+while(true)
+{
+	 System.out.println("Choose Your Option\n1.Insert into table\n2.Update table\n3.Delete row\n4.Read Table\n5.Exit ");
+	 int n1 = sc.nextInt(); 
+     if(n1 == 1 || n1 == 2 || n1 == 3 || n1 == 4)
+     {
+    	 
+		switch(n1)
+		{  
+			case 1:
+				System.out.println("Please Enter student ID");
+				int ID = sc.nextInt();
+				System.out.println("Please Enter student Name");
+				String Name = sc.next();	
+				stmt.setInt(1, ID);
+				stmt.setString(2,Name);
+				stmt.executeUpdate();
+				System.out.println("Data inserted into table");
+				break;
+			
+			case 2:
+				System.out.println("Please Enter student ID");
+				int ID1 = sc.nextInt();
+				System.out.println("Please Enter student Name");
+				String Name1 = sc.next();
+				
+				String update = "UPDATE student SET studentname="+"'"+ Name1 +"'"+ " WHERE studentid="+ ID1;
+				System.out.println(update);
+				stmt.executeUpdate(update);
+				System.out.println("Data Updated");
+				break;
+				
+			case 3:
+				System.out.println("Please Enter student ID");
+				int ID11 = sc.nextInt();
+				String delete = "DELETE FROM student " +"WHERE studentid =" + ID11;
+				stmt.executeUpdate(delete);
+				System.out.println("Data Deleted");
+				break;
+				
+			case 4:
+				ResultSet rs=stmt.executeQuery("select * from student");  
+				while(rs.next())  
+				System.out.println(rs.getInt(1)+"  "+rs.getString(2));  
+				break;
+		}
+}
+     else
+     {
+    	 break;
+     }
+}
+}catch(Exception e){ System.out.println(e);}  
+}  
+}
